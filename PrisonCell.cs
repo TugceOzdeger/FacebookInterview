98f1e91e38f385c793eded67462367784b5fd92e
Tugce <52296491+TugceOzdeger@users.noreply.github.com>
2022-04-10 23:09:26 +0200

    Add files via upload

---
+++ PrisonCell.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FacebookInterview
+{
+    public class PrisonCell
+    {
+        /*If a cell has two adjacent neighbors that are both occupied or both vacant, 
+        then the cell becomes occupied.
+        Otherwise, it becomes vacant.
+        Input: cells = [0,1,0,1,1,0,0,1], N = 7
+        Output: [0,0,1,1,0,0,0,0]
+
+        Explanation: 
+        The following table summarizes the state of the prison on each day:
+        Day 0: [0, 1, 0, 1, 1, 0, 0, 1]
+        Day 1: [0, 1, 1, 0, 0, 0, 0, 0]
+        Day 2: [0, 0, 0, 0, 1, 1, 1, 0]
+        Day 3: [0, 1, 1, 0, 0, 1, 0, 0]
+        Day 4: [0, 0, 0, 0, 0, 1, 0, 0]
+        Day 5: [0, 1, 1, 1, 0, 1, 0, 0]
+        Day 6: [0, 0, 1, 0, 1, 1, 0, 0]
+        Day 7: [0, 0, 1, 1, 0, 0, 0, 0]
+
+        Input: cells = [1,0,0,1,0,0,1,0], N = 1000000000
+        Output: [0,0,1,1,1,1,1,0]*/
+
+        public int[] PrisonAfterNDays()
+        {
+            int N = 7;
+            int[] cells = { 0, 1, 0, 1, 1, 0, 0, 1};
+            int[] newCells = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            for (var i = 0; i < N; i++)
+            {
+                var index = 0;
+
+                while (index < cells.Count() - 2)
+                {
+                    if ((cells[index] == 0 && cells[index + 2] == 0) || 
+                        (cells[index] == 1 && cells[index + 2] == 1))
+                        newCells[index + 1] = 1;                
+                    index++;
+                }
+            }
+            return newCells;
+        }
+
+        public List<int> FindUglyNumber(int n) 
+        {
+            var uglyNumbers = new List<int>{1};
+            var index = 2;
+
+            while (uglyNumbers.Count < n)
+            {
+                if (index % 2 == 0 || index % 3 == 0 || index % 5 == 0)
+                    uglyNumbers.Add(index);
+                index++;
+            }
+
+            return uglyNumbers;
+        }
+    }
+}
---
+++ SameTree.cs
@@ -0,0 +1,73 @@
+﻿namespace FacebookInterview
+{
+    public class SameTree
+    {
+        public TreeNode p { get; set; }
+        public TreeNode q { get; set; }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+         }
+
+        public bool IsSameTree()
+        {
+            //Check first if they have same height
+            if (FindHeight(p) != FindHeight(q) || FindHeight(p, true) != FindHeight(q, true))
+                return false;
+            else
+            {
+
+                //Iterate each level
+                if (p.val == q.val)
+                {
+                    if (p.left.val == q.left.val)
+                    {
+                        if (p.right.val == q.right.val)
+                        {
+                            return true;
+                        }
+                        return false;
+                    }
+                    return false;
+                }
+                return false;
+            }
+        }
+
+        private int FindHeight(TreeNode node, bool isLeft = false)
+        {
+            var height = 0;
+
+            if (isLeft)
+            {
+                var result = node?.left;
+
+                while (result != null)
+                {
+                    height++;
+                    result = result?.left ?? result?.right;
+                }
+            }
+            else
+            {
+                var result = node?.right;
+
+                while (result != null)
+                {
+                    height++;
+                    result = result?.right ?? result?.left;
+                }
+            }
+            return height;
+        }
+    }
+}
